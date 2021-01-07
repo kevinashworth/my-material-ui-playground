031cdb78fdc01eee17d5d73fc6752b6cfe8f728a
Kevin Ashworth <kevinashworth@yahoo.com>
2021-01-07 08:08:09 -0800

    Fix missing card image
    Other small tweaks

--- src/CardExample.jsx
+++ src/CardExample.jsx
@@ -1,14 +1,22 @@
 import React from 'react';
-import { Button, Card, CardActions, CardContent, CardHeader, CardMedia, Typography } from '@material-ui/core'
+import { makeStyles } from '@material-ui/core/styles';
+import { Button, Card, CardActions, CardContent, CardHeader, CardMedia, Typography } from '@material-ui/core';
 
-const CardExample = (props) => {
-  console.log('props:', props)
+const useStyles = makeStyles((theme) => ({
+  media: {
+    height: 240
+  }
+}));
+
+const CardExample = () => {
+  const classes = useStyles();
+
   return (
     <Card>
-      <CardMedia image='http://placekitten.com/320/240' />
+      <CardMedia className={classes.media} image='http://placekitten.com/320/240' />
       <CardHeader title='Card Title' />
       <CardContent>
-        <Typography variant="body2" color="textSecondary" component="p">
+        <Typography component='p'>
           Eum neque placeat et cumque voluptates? Ad deleniti dolorem <em>et dolor</em> nam consectetur Quis. Eum galisum esse <strong>nam odit non tempora explicabo id optio tempora</strong> qui vero ducimus et consequatur iste qui laudantium voluptatem.
         </Typography>
       </CardContent>
