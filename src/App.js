30e6a9ec40cc37c5c4140875630d67858c6e7e27
Kevin Ashworth <kevinashworth@yahoo.com>
2021-02-12 13:13:38 -0800

    Add basic Chip / ChipInput examples

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import React from 'react';
 import { Grid } from '@material-ui/core';
 import CardExample from './CardExample';
+import ChipExample1 from './ChipExample1';
+import ChipExample2 from './ChipExample2';
 
 function App () {
   return (
     <div className='App'>
       <Grid container spacing={2}>
-        <Grid item xs={3} />
+        <Grid item xs={3}>
+          <ChipExample1 />
+        </Grid>
         <Grid item xs={6}>
           <CardExample />
         </Grid>
-        <Grid item xs={3} />
+        <Grid item xs={3}>
+          <ChipExample2 />
+        </Grid>
       </Grid>
     </div>
   );
---
+++ src/ChipExample1.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Box } from '@material-ui/core';
+import ChipInput from 'material-ui-chip-input';
+
+const ChipExample1 = () => {
+  return (
+    <Box>
+      <ChipInput
+        defaultValue={['clown fish', 'whale', 'shark']}
+        fullWidth
+        label='Fish and chips'
+        placeholder='Type and press enter to add chips'
+      />
+    </Box>
+  );
+};
+
+export default ChipExample1;
